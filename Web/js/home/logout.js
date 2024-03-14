5b0cfecd8174d70587dab09cfe8b138e5a784611
Vinaz <125428096+vinh23052002@users.noreply.github.com>
2024-03-14 09:18:13 +0700

    Done show total view

--- Web/js/home/logout.js
+++ Web/js/home/logout.js
@@ -4,5 +4,6 @@ const Logout = () => {
   document.cookie = "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
   document.cookie = "fullName=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
   document.cookie = "phone=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  document.cookie = "userID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
   window.location.href = "login.html";
 };
